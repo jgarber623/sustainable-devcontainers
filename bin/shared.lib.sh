050985b7f418875aa1e55a0b9366288e1295a8fd
Jason Garber <jason@sixtwothree.org>
2023-10-20 01:16:26 +0000

    Add check_for_docker and run_within_docker functions

--- bin/shared.lib.sh
+++ bin/shared.lib.sh
@@ -1,9 +1,34 @@
-COMPOSE_FILE=./.devcontainer/docker-compose.yml
+COMPOSE_FILE="./.devcontainer/docker-compose.yml"
 
 log() {
   echo "[ "${0}" ]" "${@}"
 }
 
-# check_for_docker() {}
+check_for_docker() {
+  if ! command -v "docker"; then
+    log "🚨 Docker is not installed! Download and install Docker by visiting:"
+    log ""
+    log "  https://www.docker.com/get-started/"
+    log ""
+    exit 1
+  fi
+}
 
-# run_within_docker() {}
+run_within_docker() {
+  FILE="/proc/1/sched"
+
+  if ! [ -r "${FILE}" ] || ! grep -q "docker-init" "${FILE}"; then
+    log "🚨 Command \"${@}\" must be executed within the running container:"
+    log ""
+    log "   bin/exec ${@}"
+    log ""
+
+    read -p "Re-execute \"${@}\" within the running container? [yN] " rerun
+
+    if [ "${rerun,,}" = "y" ]; then
+      bin/exec "${@}"
+    else
+      exit 1
+    fi
+  fi
+}
