cae0bac6660780911c2bbb735db46a61fd0e9299
Jason Garber <jason@sixtwothree.org>
2023-10-20 15:34:04 -0400

    Refactor scripts

--- bin/shared.lib.sh
+++ bin/shared.lib.sh
@@ -1,11 +1,21 @@
-COMPOSE_FILE="./.devcontainer/docker-compose.yml"
+if [ -z $SCRIPT_DIR ]; then
+  log "🚨 SCRIPT_DIR was not defined."
+  exit 1
+fi
+
+ROOT_DIR=$(cd -- "${SCRIPT_DIR}/.." > /dev/null 2>&1 && pwd)
+COMPOSE_FILE="${ROOT_DIR}"/.devcontainer/docker-compose.yml
 
 log() {
   echo "[ "${0}" ]" "${@}"
 }
 
+title() {
+  echo -e "\033[1;37m${@}\033[0m"
+}
+
 check_for_docker() {
-  if ! command -v "docker"; then
+  if ! command -v "docker" > /dev/null 2>&1; then
     log "🚨 Docker is not installed! Download and install Docker by visiting:"
     log ""
     log "  https://www.docker.com/get-started/"
@@ -16,17 +26,20 @@ check_for_docker() {
 
 run_within_docker() {
   FILE="/proc/1/sched"
+  BIN_DIR=$(dirname -- "${0}")
 
   if ! [ -r "${FILE}" ] || ! grep -q "docker-init" "${FILE}"; then
-    log "🚨 Command \"${@}\" must be executed within the running container:"
+    log "🚨 The command \"${0}\" must be executed within the running container:"
     log ""
-    log "   bin/exec ${@}"
+    log "   ${BIN_DIR}/exec ${0} ${@}"
     log ""
 
-    read -p "Re-execute \"${@}\" within the running container? [yN] " rerun
+    read -p "[ "${0}" ] ♻️  Re-execute the command within the running container? [y/N] " yn
 
-    if [ "${rerun,,}" = "y" ]; then
-      bin/exec "${@}"
+    shopt -s nocasematch
+
+    if [ "${yn}" = "y" ]; then
+      "${BIN_DIR}"/exec $0 $@
     else
       exit 1
     fi
